0c3d4a1b56b27dc99c50f703fc3e31d2518ec8eb
Gaurav Arya <gauravarya@gaurav958.local>
2020-02-10 22:59:18 +0530

    adding initial files

---
+++ build/precache-manifest.f4b22f54ce5450508184822d28cee138.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "115cf3aee4ed88fa808d1bfd3cd68575",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2944ef798f8b4e1d79ed",
+    "url": "/monsters-rolodex/static/css/main.69a3ed99.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2944ef798f8b4e1d79ed",
+    "url": "/monsters-rolodex/static/js/main.e6c0ef0b.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
